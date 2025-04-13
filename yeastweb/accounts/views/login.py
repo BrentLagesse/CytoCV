21e78e7270f297ca650fd8c4316d9aa98a7b46d2
timmy0402 <minhnhathb04@gmail.com>
2025-04-12 19:28:00 -0700

    rebase main

--- yeastweb/accounts/views/login.py
+++ yeastweb/accounts/views/login.py
@@ -1,23 +1,23 @@
 from django.template.response import TemplateResponse
-from django.contrib.auth import authenticate, login
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import redirect
 # test user:
 # username: timmy
 # password: 12345
-from django.views.decorators.csrf import csrf_exempt
 
 def auth_login(request):
-    form = AuthenticationForm(data=request.POST)
-
     if request.method == "POST":
+        # getting username and password
         username = request.POST.get('username')
         password = request.POST.get('password')
-        print(username, password)
         user = authenticate(request, username=username, password=password)
-        if user is not None:
+        if user is not None: # login success
             login(request, user)
-            print("Login Successful")
-        else:
-            print("Login failed")
+            return redirect('profile')
+        else: # not matching credential
             return TemplateResponse(request, 'registration/login.html', {'error': 'Invalid credentials'})
     return TemplateResponse(request, "registration/login.html", {})
+
+def auth_logout(request):
+    logout(request)
+    return redirect('homepage')
