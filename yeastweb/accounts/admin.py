721494d6a0c1846230aba42ebe6aefe6efa7f192
Nicolas Gioanni <nicolasgioanni@hotmail.com>
2026-01-25 17:40:25 -0800

    Document accounts admin registration
    Add a module docstring for the custom user admin registration.

--- yeastweb/accounts/admin.py
+++ yeastweb/accounts/admin.py
@@ -1,8 +1,8 @@
+"""Admin registrations for the custom user model."""
+
 from django.contrib import admin
 from django.contrib.auth import get_user_model
 
-
 CustomUser = get_user_model()
-
 
 admin.site.register(CustomUser)
