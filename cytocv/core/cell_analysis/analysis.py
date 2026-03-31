06ff29ff7725622f7e7815d228f525841e4ae28a
Nicolas Gioanni <nicolasgioanni@hotmail.com>
2026-03-30 22:01:14 -0700

    UI fixes: added fades and clean transitions

--- cytocv/core/cell_analysis/analysis.py
+++ cytocv/core/cell_analysis/analysis.py
@@ -4,18 +4,20 @@ from core.image_processing import GrayImage
 
 from core.models import CellStatistics
 
+
 class Analysis:
     cp = None
     preprocessed_images = GrayImage()
     output_dir = None
     name = ""
 
-    def __init__(self, cp:CellStatistics=None,image:GrayImage=None, output_dir=None):
-        if(cp != None and image != None and output_dir != None):
+    def __init__(self, cp: CellStatistics = None, image: GrayImage = None, output_dir=None):
+        if cp is not None and image is not None and output_dir is not None:
             self.cp = cp
             self.preprocessed_images = image
             self.output_dir = output_dir
-    def setting_up(self, cp,preprocessed_images,output_dir):
+
+    def setting_up(self, cp, preprocessed_images, output_dir):
         self.cp = cp
         self.preprocessed_images = preprocessed_images
         self.output_dir = output_dir
